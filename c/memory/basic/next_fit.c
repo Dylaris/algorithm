6c76526d871a94913fe4025a55bb2db4d7a1a64b
Dylaris <3445818679@qq.com>
2025-03-30 18:33:20 +0800

    next fix and worst fix

--- c/memory/basic/next_fit.c
+++ c/memory/basic/next_fit.c
@@ -1,8 +1,67 @@
 #include "common.h"
 
+char mem[MEM_SIZE];
+FreeList free_list;
+static Node *start = NULL;  /* Point to the start search node */
+
+static void *nf_alloc(size_t size)
+{
+    Node *cur = free_list.dummy_head.next, 
+        *next = NULL, *prev = &free_list.dummy_head;
+    void *mem_ptr = NULL;
+
+    if (!cur) return NULL;
+
+    /* find the perv of start node */
+    while (cur != start) {
+        prev = cur;
+        cur = cur->next;
+    }
+
+    do {
+        next = cur->next;
+        if (cur->size >= size + sizeof(Header)) {
+            /* set the allocated memory chunk */
+            Header *header = (Header *) cur->ptr;
+            header->size = size;
+            header->magic = MAGIC;
+            mem_ptr = (char *) header + sizeof(Header);
+
+            /* update current node */
+            cur->size -= size + sizeof(Header);
+            cur->ptr = (char *) cur->ptr + size + sizeof(Header);
+
+            /* update free list */
+            free_list.free_size -= size + sizeof(Header);
+            if (cur->size == 0) {
+                prev->next = cur->next;
+                free(cur);
+                cur = prev->next;   /* start pointer need it */
+            }
+
+            break;
+        }
+        prev = cur;
+        cur = next;
+        if (!cur) {
+            /* a ring: start -> tail -> head -> start */
+            cur = free_list.dummy_head.next;
+            prev = &free_list.dummy_head;
+        }
+    } while (cur && cur != start);
+
+    /* update the start pointer */
+    start = cur;
+
+    return mem_ptr;
+}
+
+free_func(nf)   /* The implemention of void nf_free(void *ptr) */
+
 int main(void)
 {
     init();
+    start = free_list.dummy_head.next;
                                 /* ptr: header+sizeof(header) */
     void *ptr0 = nf_alloc(64);  /* ptr: 0+16   */
     void *ptr1 = nf_alloc(64);  /* ptr: 80+16  */
--- c/memory/basic/worst_fit.c
+++ c/memory/basic/worst_fit.c
@@ -1,5 +1,60 @@
 #include "common.h"
 
+char mem[MEM_SIZE];
+FreeList free_list;
+
+static Node *find_worst_node(size_t size, Node **prev)
+{
+    Node *cur = free_list.dummy_head.next, 
+            *next = NULL, *ptr = NULL;
+    *prev = &free_list.dummy_head;
+    size_t max_size = 0;
+
+    /* find the max size node */
+    while (cur) {
+        next = cur->next;
+        if (cur->size > max_size) {
+            max_size = cur->size;
+            ptr = cur;
+        }
+        *prev = cur;
+        cur = next;
+    }
+
+    if (!ptr || ptr->size < size) return NULL;
+
+    return ptr;
+}
+
+static void *wf_alloc(size_t size)
+{
+    void *mem_ptr = NULL;
+    Node *prev = NULL;
+    Node *cur = find_worst_node(size + sizeof(Header), &prev); 
+    if (!cur) return NULL;
+
+    /* set the allocated memory chunk */
+    Header *header = (Header *) cur->ptr;
+    header->size = size;
+    header->magic = MAGIC;
+    mem_ptr = (char *) header + sizeof(Header);
+
+    /* update current node */
+    cur->size -= size + sizeof(Header);
+    cur->ptr = (char *) cur->ptr + size + sizeof(Header);
+
+    /* update free list */
+    free_list.free_size -= size + sizeof(Header);
+    if (cur->size == 0) {
+        prev->next = cur->next;
+        free(cur);
+    }
+
+    return mem_ptr;
+}
+
+free_func(wf)   /* The implemention of void wf_free(void *ptr) */
+
 int main(void)
 {
     init();
